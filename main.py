bf93690df3ab2fe66b6d349c43f9f9a7a5a13e65
Egor Kovrizhko <kovrizhko.egor@gmail.com>
2024-08-13 19:11:54 +0900

    bot commoit

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ def say_hello(name):
     print(f'Hello my friend {name}')
 
 say_hello('Egor')
+
+def multy_num_and_word(num: int, word: str) -> str:
+    word = word.capitalize()
+    return word * num
